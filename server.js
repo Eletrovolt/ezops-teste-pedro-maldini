e6ff40ed042df6aedf0e1df1bc7ad41c56208b44
Ubuntu <ubuntu@ip-172-31-58-66.ec2.internal>
2021-04-10 21:24:24 +0000

    ATUALIZAÇÃO PEDRO

--- server.js
+++ server.js
@@ -4,67 +4,39 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var mongoose = require('mongoose');
-
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}))
-
 var Message = mongoose.model('Message',{
   name : String,
   message : String
 })
-
-var dbUrl = 'mongodb://amkurian:amkurian1@ds257981.mlab.com:57981/simple-chat'
-
+var dbUrl = 'mongodb+srv://eletro:banco123@cluster0.sjtrg.mongodb.net/simple-chat'
 app.get('/messages', (req, res) => {
   Message.find({},(err, messages)=> {
     res.send(messages);
   })
 })
-
-
-app.get('/messages/:user', (req, res) => {
-  var user = req.params.user
-  Message.find({name: user},(err, messages)=> {
+app.get('/messages', (req, res) => {
+  Message.find({},(err, messages)=> {
     res.send(messages);
   })
 })
-
-
-app.post('/messages', async (req, res) => {
-  try{
-    var message = new Message(req.body);
-
-    var savedMessage = await message.save()
-      console.log('saved');
-
-    var censored = await Message.findOne({message:'badword'});
-      if(censored)
-        await Message.remove({_id: censored.id})
-      else
-        io.emit('message', req.body);
-      res.sendStatus(200);
-  }
-  catch (error){
-    res.sendStatus(500);
-    return console.log('error',error);
-  }
-  finally{
-    console.log('Message Posted')
-  }
-
+app.post('/messages', (req, res) => {
+  var message = new Message(req.body);
+  message.save((err) =>{
+    if(err)
+      sendStatus(500);
+    io.emit('message', req.body);
+    res.sendStatus(200);
+  })
 })
-
-
-
 io.on('connection', () =>{
   console.log('a user is connected')
 })
-
 mongoose.connect(dbUrl ,{useMongoClient : true} ,(err) => {
   console.log('mongodb connected',err);
 })
-
-var server = http.listen(3000, () => {
+var server = http.listen(3001, () => {
   console.log('server is running on port', server.address().port);
 });
